74222b6393af52ffe62c0e9022b5e607d5ff4c89
Hilary Castelar <hilary.castelar555@gmail.com>
2024-05-31 18:02:12 -0300

    tipos de dados

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    //variaveis e constantes
+    public static void main(String[] args) {
+        String txt = "Hilary Castelar";
+
+        System.out.print(txt);
+
+        txt = "Cubos Academy";
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print(txt);
+
+         final String txt2 = "Minha primeira constante em Java";
+
+         System.out.print(txt2);
+         
+         //Tipos de dados
+
+        //numeros
+        //numeros inteiros
+
+        int numeroInteiro = 12;
+        System.out.println(numeroInteiro);
+        //numeros double
+        double numeroDecimal = 12.5;
+        System.out.println(numeroDecimal);
+
+        //lógicos
+        boolean verdadeiroOuFalso = true;
+        System.out.println(verdadeiroOuFalso);
+
+    }
+}
