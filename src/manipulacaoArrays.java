5c44271a77a914a83f747c4116f302319126f1a9
Hilary Castelar <hilary.castelar555@gmail.com>
2024-06-05 19:50:51 -0300

    feat: arrays simples

---
+++ src/manipulacaoArrays.java
@@ -0,0 +1,14 @@
+public class manipulacaoArrays {
+    public static void main (String [] args){
+        int []  array = {1, 234, 93};
+        //int []array2 = {1, 234, 937}pode passar dessa forma
+        //int array3[]= {1, 234, 937} dessa forma também
+
+        int[] array4 = new int [5];
+        array4 [0] = 2000;
+        array4 [1] = 2001;
+        array4 [2] = 2002;
+        System.out.println(array4[1]);
+
+    }
+}
