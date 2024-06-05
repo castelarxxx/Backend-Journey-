93e326fb61ec98444cd4014f85f3e937f66d869e
Hilary Castelar <hilary.castelar555@gmail.com>
2024-06-05 20:13:38 -0300

    feat: Como utilizar o FOR

---
+++ src/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main (String[] args){
+        int [] arraySimples = {1, 2 , 3, 4};
+
+        for(int i = 0; i < arraySimples.length; i++){
+            System.out.println(arraySimples[i]);
+        }
+        //System.out.println(arraySimples.length);
+    }
+}
+// operador ++ vai acrescentando, vai executar enquanto i for menor que array simples ( i = 0)
--- src/manipulacaoArrays.java
+++ src/manipulacaoArrays.java
@@ -2,14 +2,14 @@ public class manipulacaoArrays {
     public static void main(String[] args) {
         // int []  array = {1, 234, 93};
         //int []array2 = {1, 234, 937}pode passar dessa forma
-       // int array3[]= {1, 234, 937} dessa forma também
+        //int array3[]= {1, 234, 937} dessa forma também
 
-         int[] array4 = new int [5];
-         array4 [0] = 2000;
-         array4 [1] = 2001;
-         array4 [2] = 2002;
-        System.out.println(array4[1]);
-        String[][] arrayMultidimensional = {{"Hilary", "Castelar",},
+        // int[] array4 = new int [5];
+        // array4 [0] = 2000;
+        // array4 [1] = 2001;
+        // array4 [2] = 2002;
+        //System.out.println(array4[1]);
+       // String[][] arrayMultidimensional = {{"Hilary", "Castelar",},
                // {"Hilary", "Alves",},
                 //{"CASTELAR", "ALVES"}};
         //System.out.println(arrayMultidimensional[0][1]);
